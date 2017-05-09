a3b960fbebad65ff981f1d40acc1d31ef3cdbe1d
jcostopulos <costopulos@gmail.com>
2017-05-08 18:54:13 -0700

    All categories working with no resets

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,28 +9,44 @@ var alphabet = [ 'a', 'b', 'c', 'd', 'e',
                     'z' ];
 
 //Pull random letter from array and store it in a varaible.
+//----make getRandomLetter a function with a keypress to begin.
  var getRandomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
  console.log(getRandomLetter);
 
 console.log("Press any key to play!");
 
 //Declare starting values for wins, guesses & losses
-var guesses = 1
+var startGuesses = 10
+var guessDecrement = 10
+var lossIncrement = 1
 var wins = 0
 var losses = 0
+var recordKeys = []
 
 //Call function onkeyup
- document.onkeyup = function(event){
 
-   if (event.key !== getRandomLetter && guesses > 0) {
-     document.getElementById('guessesLeft').innerHTML -= guesses;
+document.onkeyup = function(event) {
 
-   } else if(event.key === getRandomLetter) {
-     document.getElementById('winSlot').innerHTML = (++wins);
+   var keyed = event.key;
+   recordKeys.push(keyed);
+   var keyToString = recordKeys.toString();
+   document.getElementById("recordGuesses").innerHTML = keyToString;
 
- }    else if (document.getElementById("guessesLeft").innerHTML === -10)
-    document.getElementById("lossSlot").innerHTML = (++losses);
+   console.log(guessDecrement -= 1) //Make guessDecrement subtract itself by 1/keypress
+
+  if (keyed === getRandomLetter) {
+   console.log(document.getElementById('winSlot').innerHTML = (++wins));
+
+ } else if (keyed !== getRandomLetter && guessDecrement > -1) {
+     document.getElementById('guessesSlot').innerHTML = guessDecrement;
+
+   } else if (guessDecrement === -1)
+      document.getElementById("lossSlot").innerHTML += lossIncrement;
+
+
 }
+
+
 
 
 // (guesses - 1)
