13a0f4485250257e5059d5e81e449b26c0d57d13
jcostopulos <costopulos@gmail.com>
2017-05-10 06:40:38 -0700

    final logic in place before final styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,4 @@
+
 //PSEUDOCODE
 
 //Declare alphabet array variable
@@ -10,44 +11,56 @@ var alphabet = [ 'a', 'b', 'c', 'd', 'e',
 
 //Pull random letter from array and store it in a varaible.
 //----make getRandomLetter a function with a keypress to begin.
- var getRandomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
- console.log(getRandomLetter);
+var getRandomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+console.log(getRandomLetter);
 
 console.log("Press any key to play!");
 
-//Declare starting values for wins, guesses & losses
 var startGuesses = 10
 var guessDecrement = 10
-var lossIncrement = 1
+var lossIncrement = 0
 var wins = 0
 var losses = 0
 var recordKeys = []
 
-//Call function onkeyup
+$("#play-again").on("click", function() { //jquery onclick button (callback function)
+  console.log("PLAY AGIAN button");
+    getRandomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+  console.log(getRandomLetter);
+    guessDecrement = 10;
+    startGuesses = 0;
+    recordKeys = [];
+    document.onkeyup = playGame;
+  });
 
-document.onkeyup = function playGame (event) {
+$("#restart-all").on("click", function() { //jquery onclick button (callback function)
+  console.log("clicked restart button");
+  location.reload();
+  });
 
-   var keyed = event.key;
+
+//
+//Start Game Function - press play agian button, array resets to blank
+//MOVED INTO FUNCTION - NEED TO KEEP OUTSIDE? -
+//Declare starting values for wins, guesses & losses
+
+
+//Primary game function
+  var playGame = function startGame (event) { //make this a button click and anonymous function
+
+  var keyed = event.key;
    recordKeys.push(keyed);
    var keyToString = recordKeys.toString();
    document.getElementById("recordGuesses").innerHTML = keyToString;
 
-   console.log(guessDecrement -= 1) //Make guessDecrement subtract itself by 1/keypress
+   if (keyed === getRandomLetter) {
+   document.getElementById('winSlot').innerHTML = (++wins);
 
-  if (keyed === getRandomLetter) {
-   console.log(document.getElementById('winSlot').innerHTML = (++wins));
-
- } else if (keyed !== getRandomLetter && guessDecrement > -1) {
+  } else if (keyed !== getRandomLetter && guessDecrement > 0) {
+    guessDecrement -= 1; //Make guessDecrement subtract itself by 1/keypress
      document.getElementById('guessesSlot').innerHTML = guessDecrement;
 
-   } else if (guessDecrement === -1)
-      document.getElementById("lossSlot").innerHTML += lossIncrement;
-
-
+  } if (guessDecrement === 0) {
+    document.getElementById("lossSlot").innerHTML = (++lossIncrement);
+ }
 }
-
-
-
-
-// (guesses - 1)
-// document.getElementById('guessesLeft').innerHTML =
